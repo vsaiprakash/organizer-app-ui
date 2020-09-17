1d2d000a971cebac3ede1d8b7b8814b61800f149
Vukkum Sai Prakash <vukkumsp@gmail.com>
2020-09-17 17:16:52 +0530

    Coded to present hardcoded data in todo list

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo',
@@ -6,8 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  // todo_list = this.http.get("http://localhost:3000/api/todo-list");
 
-  constructor() { }
+  todo_list =  [
+    {
+      "id": 1,
+      "title":"Grociers",
+      "list": [
+        "sugar", "dhal"
+      ]
+    },
+    {
+      "id": 2,
+      "title":"Homework",
+      "list": [
+        "social", "maths"
+      ]
+    }
+  ];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
