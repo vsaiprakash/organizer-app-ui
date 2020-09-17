2a2ea4dbbfc72636fb1bb1ee6d6843197a06dea2
Vukkum Sai Prakash <vukkumsp@gmail.com>
2020-09-17 15:12:57 +0530

    Routing added for To-Do body from Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'todo-window', component: TodoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
