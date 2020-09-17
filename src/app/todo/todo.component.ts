ff9a86d7da4dd4f62302f5d50003c050e0b4a612
Vukkum Sai Prakash <vukkumsp@gmail.com>
2020-09-17 17:18:32 +0530

    HTTPClient to fetch data; Still CORS issue to fix

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,24 +9,24 @@ import { catchError, retry } from 'rxjs/operators';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  // todo_list = this.http.get("http://localhost:3000/api/todo-list");
+  todo_list = this.http.get("http://localhost:3000/api/todo-list");
 
-  todo_list =  [
-    {
-      "id": 1,
-      "title":"Grociers",
-      "list": [
-        "sugar", "dhal"
-      ]
-    },
-    {
-      "id": 2,
-      "title":"Homework",
-      "list": [
-        "social", "maths"
-      ]
-    }
-  ];
+  // todo_list =  [
+  //   {
+  //     "id": 1,
+  //     "title":"Grociers",
+  //     "list": [
+  //       "sugar", "dhal"
+  //     ]
+  //   },
+  //   {
+  //     "id": 2,
+  //     "title":"Homework",
+  //     "list": [
+  //       "social", "maths"
+  //     ]
+  //   }
+  // ];
 
   constructor(private http: HttpClient) { }
 
