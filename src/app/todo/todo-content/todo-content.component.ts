2e02aa1f8476ad23acdfb8f580f0e63f5344d186
Vukkum Sai Prakash <vukkumsp@gmail.com>
2020-09-18 14:58:20 +0530

    Code CleanUp

--- src/app/todo/todo-content/todo-content.component.ts
+++ src/app/todo/todo-content/todo-content.component.ts
@@ -12,36 +12,35 @@ import { TodoService } from './../todo.service';
 })
 export class TodoContentComponent implements OnInit, OnChanges {
   @Input() todo_list_selected_id: number;
-  // todo_list_selected_id_obs: BehaviorSubject<number> = new BehaviorSubject(1);
   todo_list_selected: any;
   todoService: TodoService;
-  todo_list_selected_id_change_obs: any;
 
 
   constructor(todoService: TodoService) {
-
     this.todoService = todoService;
+  }
+
+  ngOnInit(): void {
     if(typeof this.todo_list_selected_id === 'undefined'){
       this.todo_list_selected_id = 1;
     }
 
-    todoService.getToDoList(this.todo_list_selected_id)
+    this.todoService.getToDoList(1)
                .subscribe(list => this.todo_list_selected = list);
+  }
 
+
+  ngOnChanges(): void{
+    if(typeof this.todo_list_selected_id === 'undefined'){
+      this.todo_list_selected_id = 1;
+    }
+    //to update todo list when selected from todo-nav
+    this.onChange();
   }
 
   onChange(){
     this.todoService.getToDoList(this.todo_list_selected_id)
     .subscribe(list => this.todo_list_selected = list);
-  }
-
-  ngOnInit(): void {
-  }
-
-
-  ngOnChanges(): void{
-    //to update todo list when selected from todo-nav
-    this.onChange();
   }
 
 }
